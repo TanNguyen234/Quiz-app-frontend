9f8a549eeb684b5e65442733e34b34bedeb47357
TanNguyen234 <tanntd.2005@gmail.com>
2024-12-20 18:15:37 +0700

    Hoàn thành thống kê answers và profile 2

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,52 +1,70 @@
 import "./profile.scss";
 import { Avatar, Button, Col, Empty, Form, Input, Row } from "antd";
-import { UserOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { auth } from "../../untils/request";
 import { getCookie } from "../../helpers/cookie";
+import swal from "sweetalert";
 
 function Profile() {
   const [info, setInfo] = useState(null);
-  const [edit, setEdit] = useState(true);
+  const [edit, setEdit] = useState(false);
+  const [form] = Form.useForm();
 
   const onFinish = (values) => {
+    console.log(edit)
     console.log("Received values of form: ", values);
+    setEdit(false)
   };
 
   useEffect(() => {
     const fetchApi = async () => {
       const response = await auth("user/detail", getCookie("token"));
-      if (response.code === 200) {
+      if(response.code === 200) {
         setInfo(response.data);
-        console.log(info)
       }
     };
     fetchApi();
   }, []);
+
+  const handleSubmit = () => {
+    swal("Bạn có chắc muốn lưu thông tin đã thay đổi?", {
+      buttons: ["Không", "Có"],
+    })
+     .then((value) => {
+      if(value) {
+        form.submit()
+      }
+     })
+  }
 
   return (
     <>
       {info ? (
         <Row className="profile" gutter={20}>
           <Col span={10} className="profile__infor">
-            <div className="profile__avatar"><Avatar shape="square" size={200} src='https://s.yimg.com/fz/api/res/1.2/cRBKAk0t85azLk6FjuUtnA--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7Zm9ybWF0PXdlYnA7aD0zODA7cHhvZmY9NTA7cHlvZmY9MDtxPTgwO3c9MjQw/https://s.yimg.com/zb/imgv1/acaa4b81-c559-38d3-946f-7d1eb829cd32/t_1024x1024.cf.webp' /></div>
-            <Form className="profile_form" disabled={edit} onFinish={onFinish}>
-            <Form.Item
+            <div className="profile__avatar">
+              <Avatar
+                shape="square"
+                src={info.avatar ? info.avatar : "/images/fate.webp"}
+              />
+            </div>
+            <Form form={form} className="profile_form" disabled={!edit} onFinish={onFinish}>
+              <Form.Item
                 name="id"
                 rules={[
                   {
-                    required: true,
+                    required: false,
                   },
                 ]}
                 label="id: "
-                style={{color: 'white'}}
+                style={{ color: "white" }}
               >
                 <Input
                   placeholder="id"
                   className="profile__input"
                   variant="borderless"
                   type="text"
-                  disabled
+                  disabled={true}
                   defaultValue={info.id}
                 />
               </Form.Item>
@@ -59,13 +77,14 @@ function Profile() {
                   },
                 ]}
                 label="User Name: "
-                style={{color: 'white'}}
+                style={{ color: "white" }}
               >
                 <Input
                   placeholder="fullName"
                   className="profile__input"
                   variant="borderless"
                   type="text"
+                  disabled={!edit}
                   defaultValue={info.fullName}
                 />
               </Form.Item>
@@ -78,22 +97,32 @@ function Profile() {
                   },
                 ]}
                 label="Email: "
-                style={{color: 'white'}}
+                style={{ color: "white" }}
               >
                 <Input
                   className="profile__input"
                   placeholder="Email"
                   variant="borderless"
                   type="email"
+                  disabled={!edit}
+                  value={info.email}
                   defaultValue={info.email}
                 />
               </Form.Item>
-              <Form.Item>
-                <Button disabled={false} color="white">Edit</Button>
-              </Form.Item>
+              {edit ? (
+                <>
+                  <Form.Item>
+                    <Button disabled={false} color="primary" variant="primary" onClick={() => handleSubmit()}>Lưu thay đổi</Button>
+                    <Button disabled={false} onClick={()=>setEdit(false)} color="white">Hủy</Button>
+                  </Form.Item>
+                </>
+              ) : (
+                <Form.Item>
+                  <Button disabled={false} onClick={()=>setEdit(true)} color="white">Chỉnh sửa</Button>
+                </Form.Item>
+              )}
             </Form>
           </Col>
-          <Col span={10} className="profile__achievement"></Col>
         </Row>
       ) : (
         <Empty />
--- src/pages/statistics/index.jsx
+++ src/pages/statistics/index.jsx
@@ -1,50 +1,80 @@
+import "./style.scss";
 import { useEffect, useState } from "react";
 import { getAnswers } from "../../services/getAnswers";
-import { Empty, Table } from "antd";
+import { Empty, Table, Tag } from "antd";
 import { getTopics } from "../../services/getTopics";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 
 function Statistic() {
-    const [data, setData] = useState([])
-    const [topics, setTopics] = useState([])
+  const [data, setData] = useState([]);
+  const [topics, setTopics] = useState([]);
 
-    useEffect(() => {
-        const fetchApi = async () => {
-            const answers = await getAnswers();
-            const topicData = await getTopics();
-            setTopics(topicData);
-            setData(answers);
-            console.log(answers, topicData);
+  useEffect(() => {
+    const fetchApi = async () => {
+      const answers = await getAnswers();
+      const topicData = await getTopics();
+      setTopics(topicData);
+      setData(answers);
+    };
+    fetchApi();
+  }, []);
+
+  const returnNameTopic = (id) => {
+    if (topics.length > 0) {
+      for (let i = 0; i < topics.length; i++) {
+        if (topics[i]._id === id) {
+          return topics[i].title;
         }
-        fetchApi()
-    }, [])
-
-    const returnNameTopic = () => {
-      
+      }
     }
-    return (<>
-    {data.length > 0 ? <>
-       <table>
-          <thead>
-            <tr>
-              <th>Chủ đề</th>
-              <th>Ngày làm</th>
-              <th>Số câu</th>
-              <th>Xem đáp án</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(item => (
-              <tr key={item._id}>
-                <td>{item.question}</td>
-                <td>{topics.find(topic => topic._id === item.topicId)?.name}</td>
-                <td>{item.correctAnswer}</td>
-                <td>{item.selectedAnswer}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-    </> : <Empty className="questions_empty" />}
-    </>)
+  };
+
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "index",
+      key: "index",
+      render: (index, _, rowIndex) => rowIndex + 1,
+    },
+    {
+      title: "Chủ đề",
+      dataIndex: "topicId",
+      key: "topicId",
+      render: (id) => returnNameTopic(id)
+    },
+    {
+      title: "Số câu",
+      dataIndex: "answers",
+      key: "answers",
+      render: (answers) => Object.keys(answers).length,
+    },
+    {
+      title: "Ngày làm",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      sorter: (a, b) => a.createdAt > b.createdAt,
+      showSorterTooltip: (a, b) => a.createdAt > b.createdAt,
+      render: (date) => dayjs(date).format("DD/MM/YYYY HH:mm:ss"),
+    },
+    {
+      title: "Hành động",
+      dataIndex: "_id",
+      key: "_id",
+      render: (id) => <Tag color="geekblue"><Link to={"/answers/check/" + id}>Xem kết quả</Link></Tag>
+    },
+  ];
+  return (
+    <>
+      {data.length > 0 ? (
+        <>
+          <Table columns={columns} dataSource={data} bordered={false} size="middle" className="table"/>;
+        </>
+      ) : (
+        <Empty className="questions_empty" />
+      )}
+    </>
+  )
 }
 
 export default Statistic;
